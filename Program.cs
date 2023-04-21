e35823e59a020b3505dbdc07c6d9b52c11d6f3f9
prashantgurav <guravp@gmail.com>
2023-04-21 17:00:29 +0530

    [Prahsnt] Besic Program

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To The Basic Core Program");
+            Console.WriteLine("Please Choose One From Below Option: ");
+            Console.WriteLine("1.FlipCoin\n 2.LeapYear\n 3.PowerOfTwo\n 4.HarmonicSeries\n 5.EvenorOdd");
+            int Option = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
